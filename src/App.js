f388922a109fd0b7de83d60cbdd90c2c5616fbc0
Narda Amador <narduk.a@gmail.com>
2019-12-02 21:23:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import Menu from './Menu';
+
+
+class App extends Component {
+    render() {
+        return( 
+            <React.Fragment>
+
+                <Menu></Menu>
+            </React.Fragment>
+        );      
+    }
+}
+
+
+export default App;
